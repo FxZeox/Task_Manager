b03b4a0fbad7e7a131b55d729db81af19711c9d6
Usman Ahmad <usmanjee522@gmail.com>
2025-02-10 11:41:37 +0500

    Setting CI pipelines and Dockerize it

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081/api', // Update this if the server runs on a different port
+  baseURL: 'http://localhost:5000/api', // Update this if the server runs on a different port
 });
 
 export default axiosInstance;
